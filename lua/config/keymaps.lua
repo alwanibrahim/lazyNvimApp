4ba88a4ebe8eea85bd0b67dca56a376b7dfd654f
alwanibrahim <alwanibrahim142@gmail.com>
2025-12-19 08:17:00 +0700

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+map("i", "jj", "<Esc>", { desc = "Exit insert (jj)" })
+map("i", "jk", "<Esc>", { desc = "Exit insert (jk)" })
+map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>th", function()
+  require("toggleterm").toggle(1, nil, nil, "horizontal")
+end, { desc = "Terminal horizontal" })
+map("n", "<leader>tv", function()
+  require("toggleterm").toggle(2, nil, nil, "vertical")
+end, { desc = "Terminal vertical" })
+map("n", "<leader>tf", function()
+  require("toggleterm").toggle(3, nil, nil, "float")
+end, { desc = "Terminal float" })
+map("n", "sh", "<cmd>split | wincmd j<CR>", { desc = "Horizontal split" })
+map("n", "sv", "<cmd>vsplit | wincmd l<CR>", { desc = "Vertical split" })
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]],
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      persist_size = true,
+      direction = "horizontal",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "rounded",
+      },
+    },
+  },
+}
