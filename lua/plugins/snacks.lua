a27f4f9ce039bd915752a0f631f6aefcdb5d4c40
alwanibrahim <alwanibrahim142@gmail.com>
2025-12-20 09:11:03 +0700

    refactore snack.lua + add photo

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,21 +15,14 @@ return {
     },
 
     dashboard = {
-      formats = {
-        key = function(item)
-          return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
-        end,
-      },
+      enabled = true,
+
       sections = {
-        { section = "terminal", cmd = "fortune -s | cowsay santaikawan", hl = "header", padding = 1, indent = 8 },
-        { title = "MRU", padding = 1 },
-        { section = "recent_files", limit = 8, padding = 1 },
-        { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
-        { section = "recent_files", cwd = true, limit = 8, padding = 1 },
-        { title = "Sessions", padding = 1 },
-        { section = "projects", padding = 1 },
-        { title = "Bookmarks", padding = 1 },
-        { section = "keys" },
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
       },
     },
   },
